1cc32c2fd225c1bae739dcf7e0c90b8f0f31b761
Jonathan Van Eenwyk <jvaneenwyk@tendrilinc.com>
2019-09-27 18:42:47 -0600

    Fix updating workflow file

--- build.sh
+++ build.sh
@@ -4,19 +4,23 @@ set -e
 
 nimble build
 
-FFMPEG=bin/ffmpeg
+cd ./bin
+rm -rf *.workflow
+
+cp -r ~/Library/Services/Shuffle\ Recording.workflow .
+
+FFMPEG=ffmpeg
 FFMPEG_FILE=ffmpeg-95111-g87ddf9f1ef.zip
 FFMPEG_URL=https://evermeet.cx/ffmpeg/$FFMPEG_FILE
 
 if [ ! -e "$FFMPEG" ]; then
-   mkdir -p bin
-   curl $FFMPEG_URL --output bin/$FFMPEG_FILE
-   unzip bin/$FFMPEG_FILE -d bin
+   curl $FFMPEG_URL --output $FFMPEG_FILE
+   unzip $FFMPEG_FILE
 fi
 
 rm -rf Shuffle\ Recording.worflow/Contents/Resources
 mkdir -p Shuffle\ Recording.workflow/Contents/Resources
 
 cp $FFMPEG Shuffle\ Recording.workflow/Contents/Resources
-cp bin/shuffle_recording Shuffle\ Recording.workflow/Contents/Resources
+cp shuffle_recording Shuffle\ Recording.workflow/Contents/Resources
 
